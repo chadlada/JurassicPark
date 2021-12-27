4b5b15fcc18bf6fca307dc8fd257d246050f289a
Chad <chadlada@gmail.com>
2021-12-27 17:23:40 -0500

    Started making app from start again. New Algo. DsiplayGreeting Menu Options

--- Program.cs
+++ Program.cs
@@ -8,21 +8,37 @@ namespace JurassicPark
     class Program
     {
         static void DisplayGreeting()
-
         {
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("    Welcome to Our Jurassic Park    ");
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.WriteLine("----------------------------------------\n");
+
+            Console.WriteLine("-*-*-*-*-*-*-*-*-MENU-*-*-*-*-*-*-*-*-\n");
+            Console.WriteLine("What would you like to do?\n");
+
+            Console.WriteLine("Collections Management:\n");
+            Console.WriteLine("(A)dd - New dinosaur to collection");
+            Console.WriteLine("(R)emove dinosaur from collection");
+            Console.WriteLine("(T)ransfer a dinosaurs enclosure\n");
+
+            Console.WriteLine("Reports:\n");
+            Console.WriteLine("(C)collection details of all dinosaurs and their properties");
+            Console.WriteLine("(S)summary of diet types");
+            Console.WriteLine("(V)iew Dinosaurs and date acquired");
+            Console.WriteLine("(Q)uit application");
+
+            Console.WriteLine("Please choose a letter from the options above, then press ENTER.\n");
+
+            // prompt user to enter their selection. Convert to Upper
+            var userChoice = Console.ReadLine().ToUpper();
         }
+
 
 
         static string PromptForString(string prompt)
         {
             Console.Write(prompt);
             var userInput = Console.ReadLine();
-
             return userInput;
         }
 
@@ -45,139 +61,8 @@ namespace JurassicPark
 
         static void Main(string[] args)
         {
-            var dinosaurs = new List<Dinosaur>();
-            var database = new DinosaurDatabase();
-
-
             DisplayGreeting();
-
-            var keepGoing = true;
-            // While the user hasn't said QUIT yet
-            while (keepGoing)
-            {
-                // Insert a blank line then prompt them and get their answer (force uppercase)
-                Console.WriteLine();
-                Console.Write("What do you want to do?\n(A)dd Dinosaur\n(D)elete Dinosaur\n(S)how all Dinosaurs\n(F)ind/Search Dinosaur\n(T)ransfer/Update\n(Q)uit:\n");
-                var choice = Console.ReadLine().ToUpper();
-
-                switch (choice)
-                {
-                    case "Q":
-                        keepGoing = false;
-                        break;
-                    case "D":
-                        DeleteEmployee(database);
-                        break;
-                    case "F":
-                        FindDino(database);
-                        break;
-                    case "S":
-                        ShowAllDinos(database);
-                        break;
-                    case "T":
-                        TransferUpdate(database);
-                        break;
-                    case "A":
-                        AddDino(database);
-                        break;
-                    default:
-                        Console.WriteLine("NOPE!");
-                        break;
-                }
-            }
         }
 
-        private static void DeleteEmployee(DinosaurDatabase database)
-        {
-            var nameToSearchFor = PromptForString("What name are you looking for? ");
-            // Dinosaur foundDino = dinosaurs.FirstOrDefault(dinosaur => dinosaur.Name == nameToSearchFor);
-
-
-            Dinosaur foundDino = database.FindOneDino(nameToSearchFor);
-
-            if (foundDino == null)
-
-            {
-                Console.WriteLine("No such dino up in here!");
-            }
-            else
-            {
-                Console.WriteLine($"{foundDino.Name} is a {foundDino.DietType}, weighs {foundDino.Weight}, and is located in enclosure # {foundDino.EnclosureNumber}");
-                var confirm = PromptForString("Are you sure? [Y/N] ").ToUpper();
-
-                if (confirm == "Y")
-                {
-                    database.DeleteDino(foundDino);
-                }
-
-            }
-        }
-
-        private static void TransferDino(DinosaurDatabase database, string choice)
-        {
-
-        }
-
-        private static void TransferUpdate(DinosaurDatabase database)
-        {
-            var nameToSearchFor = PromptForString("What name are you looking for? ");
-            Dinosaur foundDino = database.FindOneDino(nameToSearchFor);
-
-            if (foundDino == null)
-            {
-                Console.WriteLine("No such dino up in here!");
-            }
-            else
-            {
-                Console.WriteLine($"{foundDino.Name} is a {foundDino.DietType}, weighs {foundDino.Weight}, and is located in enclosure # {foundDino.EnclosureNumber}");
-                var changeChoice = PromptForString("What do you want to change?[Name/Enclosure]? ").ToUpper();
-
-                if (changeChoice == "NAME")
-                {
-                    foundDino.Name = PromptForString("What is the new name?");
-                }
-                if (changeChoice == "ENCLOSURE")
-                {
-                    foundDino.EnclosureNumber = PromptForInteger("What is the new enclosure number? ");
-                }
-
-            }
-        }
-
-        private static void FindDino(DinosaurDatabase database)
-        {
-            var nameToSearchFor = PromptForString("What name are you looking for? ");
-
-            Dinosaur foundDino = database.FindOneDino(nameToSearchFor);
-
-            if (foundDino == null)
-            {
-                Console.WriteLine("No such dino up in here!");
-            }
-            else
-            {
-                Console.WriteLine($"{foundDino.Name} is a {foundDino.DietType}, weighs {foundDino.Weight}, and is located in enclosure # {foundDino.EnclosureNumber}");
-            }
-        }
-
-        private static void AddDino(DinosaurDatabase database)
-        {
-            var dinosaur = new Dinosaur();
-            dinosaur.Name = PromptForString("Dinosaur name: ");
-            dinosaur.DietType = PromptForString("Dinosaur diet type: ");
-            dinosaur.EnclosureNumber = PromptForInteger("What is the enclosure number: ");
-            dinosaur.Weight = PromptForInteger("Dinosaur weight: ");
-            // dinosaur.WhenAcquired = DateTime (DateTime);  **HELP**
-
-            database.AddDino(dinosaur);
-        }
-
-        private static void ShowAllDinos(DinosaurDatabase database)
-        {
-            foreach (var dinosaur in database.GetAllDinos())
-            {
-                Console.WriteLine($"{dinosaur.Name} is a {dinosaur.DietType}, weighs {dinosaur.Weight}, and is located in enclosure # {dinosaur.EnclosureNumber}");
-            }
-        }
     }
 }
